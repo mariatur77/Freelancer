0faf4ce45099f4e1cab88095f06093e525981690
Maria <mariatur776@gmail.com>
2023-03-28 22:38:57 +0300

    Initial commit

---
+++ src/main/java/ru/netology/services/Freelancer.java
@@ -0,0 +1,23 @@
+package ru.netology.services;
+
+public class Freelancer {
+
+    public int calculate(int income, int expenses, int threshold) {
+        int counter = 0;
+        int money = 0;
+
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                //отдых
+                money = money - expenses;
+                money = money / 3;
+                counter ++;
+            } else {
+                //работа
+                money = money + income - expenses;
+            }
+        }
+        return counter;
+
+    }
+}
